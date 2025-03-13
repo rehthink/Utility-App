554a0acfde91665b2d6ca681bce61f3008ef89be
Rehan <rehnk007@gmail.com>
2025-03-14 02:39:57 +0530

    --fix

--- tracker.py
+++ tracker.py
@@ -185,20 +185,20 @@ with tab1:
                 # **📌 Display Metrics in Two Rows**
                 st.markdown(
                     f"""
-                        <div style="display: flex; justify-content: center; gap: 80px;">
+                        <div style="display: flex; justify-content: center; gap: 10px;">
                             <div style="text-align: center;">
-                                <p style="font-weight: bold; font-size: 20px;">💲 Total Due By Meter</p>
+                                <p style="font-weight: bold; font-size: 20px;">💲 Generated Bill</p>
                                 <p style="font-size: 16px; text-align: center;">₹{total_amount_per_meter:,.2f}</p>
                             </div>
                             <div style="text-align: center;">
-                                <p style="font-weight: bold; font-size: 20px;">📊 Total Utilized Units</p>
+                                <p style="font-weight: bold; font-size: 20px;">📊 Utilized Units</p>
                                 <p style="font-size: 16px; text-align: center;">{total_utilized_units:,.2f} kWh</p>
                             </div>
                         </div>
 
                         <br>
 
-                        <div style="display: flex; justify-content: center; gap: 80px;">
+                        <div style="display: flex; justify-content: center; gap: 10px;">
                             <div style="text-align: center;">
                                 <p style="font-weight: bold; font-size: 20px;">💰 Outstanding Due</p>
                                 <p style="font-size: 16px; text-align: center;">₹{total_outstanding_due:,.2f}</p>
